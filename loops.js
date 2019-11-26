7c17334e5bdc39d40e6ab292e05a13555a7f6441
Rahul <rahulramesh26@yahoo.com>
2019-11-26 23:27:15 +0530

    used while loops

---
+++ loops.js
@@ -0,0 +1,18 @@
+var count = 0;
+while( count < 6){
+    console.log("the count is : " + count );
+    count++;
+}
+//incrementing by three nos.
+var count = 0;
+while( count <= 30){
+    console.log("the count is : " + count );
+    count+=3;
+}
+
+var number=0;
+var str="hello";
+while( number< str.length){
+    console.log (str[number]);
+    number++ ;
+}
